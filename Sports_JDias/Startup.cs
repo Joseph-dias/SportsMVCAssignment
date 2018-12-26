6d3a3efea9f368128794078543bd5e7263a2cbbe
Joseph-dias <38766207+Joseph-dias@users.noreply.github.com>
2018-12-26 08:50:52 -0800

    Add project files.

---
+++ Sports_JDias/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sports_JDias.Startup))]
+namespace Sports_JDias
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
